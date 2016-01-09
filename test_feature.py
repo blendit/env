e88cd99b1421a880535768aff791b6f4948c8953
Raphaël Monat <raphael.monat@ens-lyon.org>
2016-01-09 21:45:03 +0100

    Added test case on featureline

--- test_feature.py
+++ test_feature.py
@@ -1,24 +1,37 @@
 import unittest
-from feature import Feature
+from feature import Feature, FeatureLine
 import shapely.geometry as geom
+
+class FeatureTest(Feature):
+    def __init__(self, x1, y1, x2, y2):
+        self.shape = geom.box(x1, y1, x2, y2)
+
 
 class TestFeature(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestFeature, self).__init__(*args, **kwargs)
-        self.f1 = Feature()
-        self.f1.shape = geom.box(0, 0, 1, 1)
-        self.f2 = Feature()
-        self.f2.shape = geom.box(1, 1, 2, 2)
-        self.f3 = Feature()
-        self.f3.shape = geom.box(0, 0, 2, 2)
+        self.f1 = FeatureTest(0, 0, 1, 1)
+        self.f2 = FeatureTest(1, 1, 2, 2)
+        self.f3 = FeatureTest(0, 0, 2, 2)
+
+        self.l1 = FeatureLine([(0, 0), (2, 2)], 0.1)
+        self.l2 = FeatureLine([(0, 0), (-1, -1)], 0.1)
         
     def test_intersect(self):
         self.assertFalse(self.f1.intersect(self.f2))
         self.assertFalse(self.f2.intersect(self.f1))
+
         self.assertTrue(self.f1.intersect(self.f3))
+        self.assertTrue(self.f3.intersect(self.f1))
+
         self.assertTrue(self.f2.intersect(self.f3))
         self.assertTrue(self.f3.intersect(self.f2))
-        self.assertTrue(self.f3.intersect(self.f1))
+
+        self.assertTrue(self.l1.intersect(self.f3))
+        self.assertTrue(self.f3.intersect(self.l1))
+
+        self.assertFalse(self.l2.intersect(self.f1))
+        self.assertFalse(self.f1.intersect(self.l2))
 
 
 if __name__ == '__main__':
