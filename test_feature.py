87792b87473c84f565d1ff12ef3f3b99a458a33f
Raphaël Monat <raphael.monat@ens-lyon.org>
2016-01-09 21:48:58 +0100

    clarifications

--- test_feature.py
+++ test_feature.py
@@ -14,10 +14,11 @@ class TestFeature(unittest.TestCase):
         self.f2 = FeatureTest(1, 1, 2, 2)
         self.f3 = FeatureTest(0, 0, 2, 2)
 
-        self.l1 = FeatureLine([(0, 0), (2, 2)], 0.1)
-        self.l2 = FeatureLine([(0, 0), (-1, -1)], 0.1)
+        self.l1 = FeatureLine([(0, 0), (2, 2)], 1)
+        self.l2 = FeatureLine([(0, 0), (-1, -1)], 1)
         
-    def test_intersect(self):
+    def test_intersect_ff(self):
+        """Intersection between two features"""
         self.assertFalse(self.f1.intersect(self.f2))
         self.assertFalse(self.f2.intersect(self.f1))
 
@@ -27,11 +28,18 @@ class TestFeature(unittest.TestCase):
         self.assertTrue(self.f2.intersect(self.f3))
         self.assertTrue(self.f3.intersect(self.f2))
 
+    def test_intersect_fl(self):
+        """Intersection feature/featureline"""
         self.assertTrue(self.l1.intersect(self.f3))
         self.assertTrue(self.f3.intersect(self.l1))
 
         self.assertFalse(self.l2.intersect(self.f1))
         self.assertFalse(self.f1.intersect(self.l2))
+
+    def test_intersect_ll(self):
+        """Intersection featureline/featureline"""
+        self.assertFalse(self.l1.intersect(self.l2))
+        self.assertFalse(self.l2.intersect(self.l1))
 
 
 if __name__ == '__main__':
