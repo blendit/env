094a51e4ef3107f9da84e1d434637581ef1d6937
Raphaël Monat <raphael.monat@ens-lyon.org>
2016-02-04 09:09:37 +0100

    Modifications

--- src/height_map.py
+++ src/height_map.py
@@ -5,11 +5,11 @@ import numpy
 class HeightMap:
     """Height map structure. This is just a 2D array containing z-values"""
 
-    def default_z(pos):
+    def __default_z(pos):
         """Default initialization of the map: every point at z = 0"""
         return 0
     
-    def __init__(self, sx, sy, z_func=default_z):
+    def __init__(self, sx, sy, z_func=__default_z):
         """We can pass another function to z_func to initialize directly to z_func((x,y))"""
         self.size_x = sx
         self.size_y = sy
