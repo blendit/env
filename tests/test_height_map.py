6e96e28c7e220d5e7cf3e14154b8bf39369553aa
Etienne Moutot <etienne.moutot@ens-lyon.fr>
2016-02-17 10:07:14 +0100

    Start testing heightmap for featureTree

--- tests/test_height_map.py
+++ tests/test_height_map.py
@@ -3,6 +3,8 @@ import unittest
 from src.height_map import HeightMap
 import shapely.geometry as geom
 
+from tests.test_feature_tree import FeatureTest, FeatureTestReplace
+from src.feature_tree import *
 
 class TestHeightMap(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -18,3 +20,18 @@ class TestHeightMap(unittest.TestCase):
     def test_export(self):
         self.assertRaises(None, self.m1.export("m1.png"))
         self.assertRaises(None, self.m2.export("m2.png"))
+
+class TestHeightMapTree(unittest.TestCase):
+    def test_tree_hmap(self):
+        f1 = FeatureTest(10, influence="notall")
+        f2 = FeatureTest(100, influence="notall")
+
+        f1.shape = geom.box(0, 0, 20, 20)
+        f2.shape = geom.box(50, 50, 60, 60)
+
+        tree = FeatureTree([f1, f2])
+        
+        heightmap = HeightMap(100, 100, tree.z)
+
+        self.assertRaises(None, heightmap.export("mtree.png"))
+        
