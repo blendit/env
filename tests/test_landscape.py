88d130934501c1271137619770b13d5eafc4a7c9
Etienne Moutot <etienne.moutot@ens-lyon.fr>
2016-02-23 21:32:24 +0100

    Tests for the roads

--- tests/test_landscape.py
+++ tests/test_landscape.py
@@ -1,7 +1,14 @@
 import numpy
 import unittest
-from src.landscape import Mountain
+
+from src.landscape import Mountain, Road, RoadNetwork
+from src.height_map import HeightMap
+from src.feature_tree import FeatureTree
+
 import shapely.geometry as geom
+from PIL import Image
+
+from tests.test_feature_tree import FeatureTest
 
 
 class TestMountain(unittest.TestCase):
@@ -20,3 +27,25 @@ class TestMountain(unittest.TestCase):
 
     def test_influence_weight(self):
         self.assertEqual(self.m1.influence_weight((0, 0)), 1)
+
+
+class TestRoads(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestRoads, self).__init__(*args, **kwargs)
+
+        self.background = FeatureTest(100, "notall", 1)
+        self.r1 = Road([(40,40), (70,50)], 10, 100)
+        self.r2 = Road([(20,20), (60,60), (20, 70)], 10, 100)
+
+    def test_basic_road(self):
+        hm1 = HeightMap(100, 100, self.r2.z)
+        hm1.export("mroad1.png")
+
+        original = Image.open("tests/img/mroad1.png")
+        gen = Image.open("mroad1.png")
+        self.assertEqual(original, gen)
+
+    def test_two_roads(self):
+        tree = FeatureTree([self.background, self.r1, self.r2])
+        hm2 = HeightMap(80, 80, tree.z)
+        
