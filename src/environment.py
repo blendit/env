0c5c75fd3f17da679408f9a922134d5fb6ea2482
Etienne Moutot <etienne.moutot@ens-lyon.fr>
2016-03-11 09:44:11 +0100

    Basic Environment with heightmap

--- src/environment.py
+++ src/environment.py
@@ -1,5 +1,21 @@
+from feature_tree import FeatureTree
+from height_map import HeightMap
+
+
+
 class Environment:
     """Environment class"""
 
-    def __init__(self):
-        pass
+    def __init__(self, features):
+        self.tree = FeatureTree(features)
+        self.heightmap_init = False
+
+    def init_heightmap(res_x, res_y):
+        self.heightmap = HeightMap(res_x, res_y, self.tree)
+        self.heightmap_init = True
+
+    def export_heightmap(filename, res_x=500, res_t=500):
+        if not self.heightmap_init:
+            slelf.init_heightmap(res_x, res_y)
+        
+        self.heightmap.export(filename)
