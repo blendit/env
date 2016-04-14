4110208833c146961c82e5135cddc61d57617b58
rmonat <raphael.monat@ens-lyon.org>
2016-04-14 21:35:55 +0200

    blu

--- src/blender/blend_environment.py
+++ src/blender/blend_environment.py
@@ -98,7 +98,7 @@ class BlendEnvironment(Environment):
             x, y, z = model.pos3D
             x = x - self.pos_x
             y = - y + self.pos_y
-            z = z * 10 / 255
+            z = z * 10  / 255
             bpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked": False, "mode": 'TRANSLATION'}, TRANSFORM_OT_translate={"value": (x / scaling, y / scaling, z), "constraint_axis": (False, False, False), "constraint_orientation": 'GLOBAL', "mirror": False, "proportional": 'DISABLED', "proportional_edit_falloff": 'SMOOTH', "proportional_size": 1, "snap": False, "snap_target": 'CLOSEST', "snap_point": (0, 0, 0), "snap_align": False, "snap_normal": (0, 0, 0), "gpencil_strokes": False, "texture_space": False, "remove_on_cancel": False, "release_confirm": False})
 
             self.models.append((amodel.size, bpy.context.selected_objects))
