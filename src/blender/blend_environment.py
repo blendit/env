1577c1f2d92a1b18af41d6fb26ccb83b624f6828
rmonat <raphael.monat@ens-lyon.org>
2016-04-14 22:35:56 +0200

    blu

--- src/blender/blend_environment.py
+++ src/blender/blend_environment.py
@@ -56,7 +56,7 @@ class BlendEnvironment(Environment):
             
             bpy.ops.object.modifier_add(type='DISPLACE')
             ob.modifiers["Displace"].texture = bpy.data.textures[image]
-            ob.modifiers["Displace"].strength = 1
+            ob.modifiers["Displace"].strength = 0.5
             ob.modifiers["Displace"].texture_coords = 'UV'
             ob.modifiers["Displace"].mid_level = 0
             #bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Displace")
